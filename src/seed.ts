cdba24be706691fb64067e41a65164a1b018983b
Static Variable James <staticvariablejames@gmail.com>
2020-12-21 23:36:08 -0600

    Handle changing GC outcome.

--- src/seed.ts
+++ src/seed.ts
@@ -14,6 +14,29 @@ enum GCOutcome { // Golden Cookie outcome
     // These are in the same order that they show up in the HTML file
 };
 
+const SuccessGCOutcomes = [
+    GCOutcome.Frenzy,
+    GCOutcome.Lucky,
+    GCOutcome.ClickFrenzy,
+    GCOutcome.CookieStorm,
+    GCOutcome.Blab,
+    GCOutcome.BuildingSpecial,
+    GCOutcome.CookieStormDrop,
+    GCOutcome.Sweet,
+];
+
+const BackfireGCOutcomes = [
+    GCOutcome.Sweet,
+    GCOutcome.Clot,
+    GCOutcome.Ruin,
+    GCOutcome.CursedFinger,
+    GCOutcome.ElderFrenzy,
+];
+
+const AllGCOutcomes = Object.keys(GCOutcome)
+    .filter(key => !isNaN(Number(key)))
+    .map(key => Number(key) as GCOutcome);
+
 /* Represents an outcome from a FtHoF cast.
  * seasonalVariantIndex the shape of the golden cookie during a season.
  * If season == '', then seasonalVariantIndex is ignored.
@@ -32,6 +55,7 @@ class FtHoFOutcomeFrame {
     private div: HTMLDivElement;
     private successIcon: HTMLDivElement;
     private backfireIcon: HTMLDivElement;
+    private gcOutcomeDivs: HTMLDivElement[];
 
     private outcome: FtHoFOutcome = new FtHoFOutcome();
 
@@ -41,6 +65,10 @@ class FtHoFOutcomeFrame {
         this.successIcon = this.div.querySelector('.outcome-success-icon') as HTMLDivElement;
         this.backfireIcon = this.div.querySelector('.outcome-backfire-icon') as HTMLDivElement;
 
+        this.gcOutcomeDivs = Array.from(
+            this.div.querySelector('.gc-effect-list')!.children
+        ) as HTMLDivElement[];
+
         this.addHandlers();
     }
 
@@ -48,17 +76,45 @@ class FtHoFOutcomeFrame {
         return this.outcome;
     }
 
+    private showGCOutcomes(list: GCOutcome[]) {
+        for(let i of list) {
+            this.gcOutcomeDivs[i].classList.remove('inactive');
+        }
+    }
+    private hideGCOutcomes(list: GCOutcome[]) {
+        for(let i of list) {
+            this.gcOutcomeDivs[i].classList.add('inactive');
+        }
+    }
+
     private addHandlers() {
         this.successIcon.addEventListener('click', () => {
             this.successIcon.classList.add('selected');
             this.backfireIcon.classList.remove('selected');
+            this.hideGCOutcomes(BackfireGCOutcomes);
+            this.showGCOutcomes(SuccessGCOutcomes);
+            // Correct order for handling GCOutcomes.Sweet
             this.outcome.backfire = false;
         });
         this.backfireIcon.addEventListener('click', () => {
             this.successIcon.classList.remove('selected');
             this.backfireIcon.classList.add('selected');
+            this.hideGCOutcomes(SuccessGCOutcomes);
+            this.showGCOutcomes(BackfireGCOutcomes);
+            // Correct order for handling GCOutcomes.Sweet
             this.outcome.backfire = true;
         });
+
+        for(let i of AllGCOutcomes) {
+            this.gcOutcomeDivs[i].addEventListener('click', () => {
+                // brute-force implementation
+                for(let div of this.gcOutcomeDivs) {
+                    div.classList.remove('selected');
+                }
+                this.gcOutcomeDivs[i].classList.add('selected');
+                this.outcome.gcOutcome = i;
+            });
+        }
     }
 }
 
