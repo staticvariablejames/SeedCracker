7b17c9d7aee76b95bc19eba5826247e8d6179d08
Static Variable James <staticvariablejames@gmail.com>
2020-12-23 23:06:15 -0600

    Mock seed cracking.

--- src/seed.ts
+++ src/seed.ts
@@ -255,5 +255,38 @@ class OutcomeList {
         this.buttonDiv.click();
     }
 
-    static instance = new OutcomeList();
+    private static instance = new OutcomeList();
+
+    static getOutcomes() {
+        return OutcomeList.instance.outcomeFrames.map( frame => frame.getOutcome() );
+    }
+}
+
+function crackSeed(list: FtHoFOutcome[]) {
+    // TODO: actually implement this
+    if(list.length < 5) return ['aaaaa', 'bbbbb', 'ccccc'];
+    if(list.length == 5) return ['aaaaa'];
+    return [];
+}
+
+class CrackSeedButton {
+    static instance = new CrackSeedButton();
+    private div = document.getElementById('cracking-result') as HTMLDivElement;
+    private p = this.div.querySelector('p') as HTMLParagraphElement;
+    private constructor() {
+        this.div.addEventListener('click', () => {
+            let seeds = crackSeed(OutcomeList.getOutcomes());
+            if(seeds.length > 1) {
+                this.p.textContent = `${seeds.length} viable seeds.
+                    Cast FtHoF another time and click here again!`;
+            } else if(seeds.length == 1) {
+                this.p.textContent = `Seed cracked! Your seed is: ${seeds[0]}`;
+            } else {
+                this.p.textContent = `Something went wrong,
+                    no viable seed found...
+                    Double-check that the sequence of casts is correct,
+                    reload the page, and try again.`;
+            }
+        });
+    }
 }
