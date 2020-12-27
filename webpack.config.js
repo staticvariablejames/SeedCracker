29bdbeb62f6ebdea5c5689efc41db4ff414afbad
Static Variable James <staticvariablejames@gmail.com>
2020-12-26 22:58:57 -0600

    webpack.config.js: Add webpack-dev-server settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/main.ts',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    devtool: 'inline-source-map',
+    devtool: 'eval-source-map',
+    devServer: {
+        publicPath: '/dist/',
+    },
     module: {
         rules: [
             { test: /.ts$/, use: 'ts-loader', exclude: /node_modules/ },
