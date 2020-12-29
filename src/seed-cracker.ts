71f4faaca318578e1ad57bb9d72579d3144dc321
Static Variable James <staticvariablejames@gmail.com>
2020-12-28 23:10:13 -0600

    Fully implement the seed cracking algorithm.

--- src/seed-cracker.ts
+++ src/seed-cracker.ts
@@ -1,3 +1,4 @@
+import { GCOutcome } from './gcoutcome';
 import { FtHoFOutcome } from './fthof-outcome';
 import seedrandom from 'seedrandom';
 
@@ -38,7 +39,44 @@ export function isCompatible(outcome: FtHoFOutcome, seed: string) {
     if( (prng() < 0.15) != outcome.backfire )
         return false;
 
-    return true;
+    // Generate the golden cookie
+    if(outcome.season != '') {
+        let variant;
+        if(outcome.season == 'easter') {
+            variant = Math.floor(prng() * 4);
+        } else {
+            variant = Math.floor(prng() * 8);
+        }
+        // In both branches, prng is called exactly once
+        if(outcome.seasonalVariantIndex != undefined) {
+            if(variant != outcome.seasonalVariantIndex)
+                return false;
+        }
+        // The variant number does not depend on success/backfire status
+    }
+    prng(); prng(); // x, y coordinates (not used here)
+
+    let gcChoices: GCOutcome[] = [];
+
+    if(!outcome.backfire) {
+        gcChoices.push(GCOutcome.Frenzy, GCOutcome.Lucky);
+        gcChoices.push(GCOutcome.ClickFrenzy);
+        if(prng() < 0.1) gcChoices.push(
+            GCOutcome.CookieStorm, GCOutcome.CookieStorm, GCOutcome.Blab
+        );
+        if(prng() < 0.25) gcChoices.push(GCOutcome.BuildingSpecial);
+        if(prng() < 0.15) gcChoices=[GCOutcome.CookieStormDrop];
+        if(prng() < 0.0001) gcChoices.push(GCOutcome.Sweet);
+    } else {
+        gcChoices.push(GCOutcome.Clot, GCOutcome.Ruin);
+        if(prng() < 0.1) gcChoices.push(GCOutcome.CursedFinger, GCOutcome.ElderFrenzy);
+        if(prng() < 0.003) gcChoices.push(GCOutcome.Sweet);
+        if(prng() < 0.1) gcChoices=[GCOutcome.Blab];
+    }
+
+    let gcOutcome = gcChoices[Math.floor(prng() * gcChoices.length)];
+
+    return gcOutcome == outcome.gcOutcome;
 }
 
 function isCompatibleWithAll(outcomes: FtHoFOutcome[], seed: string) {
