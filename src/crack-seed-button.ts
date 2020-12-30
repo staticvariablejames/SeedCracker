8c39927e2a184d7a112642a9d51014de39c6a273
Static Variable James <staticvariablejames@gmail.com>
2020-12-29 21:37:22 -0600

    Use an actual "crack!" button for cracking the seed.

--- src/crack-seed-button.ts
+++ src/crack-seed-button.ts
@@ -4,17 +4,17 @@ import { OutcomeList } from './outcome-list';
 export class CrackSeedButton {
     static instance = new CrackSeedButton();
     private div = document.getElementById('cracking-result') as HTMLDivElement;
-    private p = this.div.querySelector('p') as HTMLParagraphElement;
+    private button = document.getElementById('crack-seed-button') as HTMLButtonElement;
     private constructor() {
-        this.div.addEventListener('click', () => {
+        this.button.addEventListener('click', () => {
             let seeds = crackSeed(OutcomeList.getOutcomes());
             if(seeds.length > 1) {
-                this.p.textContent = `${seeds.length} viable seeds.
+                this.div.textContent = `${seeds.length} viable seeds.
                     Cast FtHoF another time and click here again!`;
             } else if(seeds.length == 1) {
-                this.p.textContent = `Seed cracked! Your seed is: ${seeds[0]}`;
+                this.div.textContent = `Seed cracked! Your seed is: ${seeds[0]}`;
             } else {
-                this.p.textContent = `Something went wrong,
+                this.div.textContent = `Something went wrong,
                     no viable seed found...
                     Double-check that the sequence of casts is correct,
                     reload the page, and try again.`;
