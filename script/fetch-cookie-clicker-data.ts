0d37808058104682f27d782f8075e69af9ba10f2
Static Variable James <staticvariablejames@gmail.com>
2021-01-30 19:05:44 -0600

    fetch-cookie-clicker-data.ts: Make the script a bit more robust.

--- script/fetch-cookie-clicker-data.ts
+++ script/fetch-cookie-clicker-data.ts
@@ -17,14 +17,17 @@ setTimeout(async () => {
     let browser = await firefox.launch();
     let page = await browser.newPage();
     let path: string;
+    let currentURL: string;
     page.on('response', async response => {
-        if(Math.floor(response.status()/100) == 2) { // Success
+        if(Math.floor(response.status()/100) == 2 && response.url() == currentURL) { // Success
+            console.log('Writing ' + path);
             writeFileSync(path, await response.body());
         }
     })
     for(let {url} of urls) {
+        currentURL = baseURL + url;
         path = 'cache/' + url;
-        await page.goto(baseURL + url);
+        await page.goto(currentURL);
     }
     await page.close();
     await browser.close();
